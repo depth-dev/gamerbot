9b778c846a526fb55c905aa768d8b449d85c3231
misterdepth <62363912+misterdepth@users.noreply.github.com>
2020-08-08 20:51:01 -0700

    began development

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const prefix = 'gb!'
+const fetch = require('node-fetch')
+const noPermsBad = new Discord.MessageEmbed()
+ .setColor('03fcd3')
+ .setTitle('No Perms')
+ .setDescription('You don\'t have perms! You are no longer gamer!')
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+    client.user.setActivity('games', { type:'PLAYING' })
+})
+client.on('message', function(message) {
+    if(message.content == 'gb!ping') {
+        if(message.author.bot) return;
+        if(message.channel.type == "dm") return;
+        const botPing = new Date() - message.createdAt;
+        message.channel.send('Pong!\nBot Latency: ' + botPing)
+    }
+})
+
+client.login('NzQxODYzNzA4MDExMjY2MDY5.Xy9waw.0sX_pC20d09dIXyZyB77P9-v3MI')
